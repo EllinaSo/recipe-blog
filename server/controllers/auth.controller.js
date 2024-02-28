a8c6f93a32e7fc9a64d598b2023ca5f25e52cbc7
Ellina S <ellina.ibr@gmail.com>
2024-02-28 14:15:55 -0600

    Added sign in functionality to server

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
 
@@ -16,6 +17,35 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(200).json('Signup successful');
+  } catch (error) {
+    next(error);
+  }
+};
+
+const signinError = 'Incorrect username or password';
+export const signin = async (req, res, next) => {
+  const { password, email } = req.body;
+
+  if (!(email && password)) {
+    return next(errorHandler(400, 'All fields are required'));
+  }
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!(user)) {
+      return next(errorHandler(404, signinError));
+    }
+
+    const { password: hashedPassword, ...userData } = user._doc;
+    if (!(bcryptjs.compareSync(password, hashedPassword))) {
+      return next(errorHandler(404, signinError));
+    }
+
+    const token = jwt.sign({ id: userData._id }, process.env.JWT_SECRET);
+    res.status(200)
+      .cookie('access_token', token, { httpOnly: true })
+      .json(userData);
   } catch (error) {
     next(error);
   }

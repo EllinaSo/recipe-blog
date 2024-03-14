787e0df2cd2cdea0d0b2d70f81afadf9a8881330
Ellina S <ellina.ibr@gmail.com>
2024-03-14 13:16:15 -0500

    Updated recipe list API

--- server/controllers/recipes.controller.js
+++ server/controllers/recipes.controller.js
@@ -2,14 +2,43 @@ import Recipe from '../models/recipe.model.js';
 
 export const get = async (req, res, next) => {
   try {
-    const recipes = await Recipe.find().sort({ createdAt: 1 }).select({
-      _id: 1,
-      preview: 1,
-      description: 1,
-      categories: 1,
-      title: 1
-    });
-    res.status(200).json(recipes);
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 6;
+    const sortDirection = req.query.order === 'asc' ? 1 : -1;
+    const shortVersion = req.query.ver !== 'full';
+
+    const recipes = await Recipe
+      .find(
+        {
+          ...(req.query.userId && { userId: req.query.userId }),
+          ...(req.query.category && { category: { $in: req.query.category } }),
+          ...(req.query.search && {
+            $or: [
+              { title: { $regex: req.query.search, $options: 'i' } },
+              { description: { $regex: req.query.search, $options: 'i' } },
+              { instructions: { $elemMatch: { $regex: req.query.search, $options: 'i' } } }
+            ]
+          })
+        }
+      )
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex * limit)
+      .limit(limit)
+      .select(shortVersion
+        ? {
+            _id: 1,
+            preview: 1,
+            description: 1,
+            categories: 1,
+            title: 1,
+            createdAt: 1,
+            updatedAt: 1
+          }
+        : {});
+
+    const total = await Recipe.countDocuments();
+
+    res.status(200).json({ recipes, total });
   } catch (error) {
     next(error);
   }
